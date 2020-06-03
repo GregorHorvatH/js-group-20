6c375a2ecdeb91bd987dff4ddb06201d4af44346
Geri <gregor.horvath83@gmail.com>
2020-06-03 21:04:11 +0300

    07-dom-events

--- src/index.js
+++ src/index.js
@@ -1,30 +1,129 @@
 'use strict';
 
 // ============================
+// bind
+// ============================
+// function sayName() {
+//   console.log(this); // {name: "Some name"}
+//   console.log(this.name);
+// }
+
+// const obj = {
+//   name: 'Some name',
+// };
+
+// // obj.sayName();
+
+// const myFunc = sayName.bind(obj); // function() { ... } // this = obj
+
+// setTimeout(myFunc, 2000);
+
+// ============================
 // addEventListener
 // ============================
+// const form = document.querySelector('.form-inline');
+// form.addEventListener('submit', () => {
+//   console.log('submit');
+// });
+
+// const fn1 = () => console.log('click');
+// const fn2 = () => console.log('csdfgsdgfd');
+
+// const myButton = document.querySelector('#my-button');
+// myButton.addEventListener('click', fn1);
+// myButton.addEventListener('click', fn2);
+
+// const handleInputChange = e => {
+//   console.log('on input:', e.target.value);
+
+//   myButton.textContent = e.target.value;
+// };
+
+// const input = document.querySelector('#inputText');
+// input.addEventListener('input', handleInputChange);
 
 // ============================
 // removeEventListener
 // ============================
+// const fn1 = () => console.log('click');
+// const fn2 = () => console.log('csdfgsdgfd');
+
+// const myButton = document.querySelector('#my-button');
+// myButton.addEventListener('click', fn1);
+// myButton.addEventListener('click', fn2);
+
+// myButton.removeEventListener('click', fn2);
 
 // ============================
 // preventDefault
 // ============================
+// const form = document.querySelector('.form-inline');
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   console.log('submit');
+// });
+
+// const link = document.querySelector('#link');
+// link.addEventListener('click', e => {
+//   e.preventDefault();
+
+//   console.log('no google :)');
+//   window.location.replace('https://goit.ua');
+// });
 
 // ============================
 // Keyboard events
 // ============================
+// const filter = document.querySelector('#filter');
+// filter.addEventListener('keydown', e => {
+//   if (e.code === 'Escape') {
+//     console.log(e.target.value);
+//   }
+// });
 
 // ============================
 // focus, blur
 // ============================
+// const filter = document.querySelector('#filter');
+// filter.addEventListener('focus', () => console.log('focus'));
+// filter.addEventListener('blur', () => console.log('blur'));
 
 // ============================
 // change, input
 // ============================
+// const filter = document.querySelector('#filter');
+// filter.addEventListener('change', () => console.log('change'));
+// filter.addEventListener('input', () => console.log('input'));
 
 // ============================
 // onLoad
 // ============================
+// window.onload = function() {
+//   const filter = document.querySelector('#filter');
 
+//   console.log(filter);
+//   // filter.addEventListener('change', () => console.log('change'));
+//   // filter.addEventListener('input', () => console.log('input'));
+// };
+
+// ============================
+// this
+// ============================
+// const obj = {
+//   name: 'Bobby',
+
+//   handleClick() {
+//     console.log(this.name);
+//   },
+// };
+
+// // obj.handleClick();
+
+// const button = document.querySelector('#my-button');
+// button.addEventListener('click', obj.handleClick.bind(obj));
+
+window.unload = function() {
+  this.alert('hello');
+  return 'ha-ha-ha';
+};
---
+++ src/taskManager.js
@@ -0,0 +1,115 @@
+'use sctrict';
+
+let tasks = [
+  {
+    id: 1,
+    text: 'lorem ipsum 1',
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: 'lorem ipsum 2',
+    isDone: false,
+  },
+  {
+    id: 3,
+    text: 'lorem ipsum 3',
+    isDone: false,
+  },
+  {
+    id: 4,
+    text: 'lorem ipsum 4',
+    isDone: true,
+  },
+];
+
+tasks = JSON.parse(localStorage.getItem('tasks')) || tasks;
+
+const getItem = (id, text, isDone) => `
+  <li data-id=${id}
+      class="list-group-item ${isDone ? 'list-group-item-success' : ''}">
+    <span>${text}</span>
+    <button type="button" class="close" aria-label="Close">
+      &times;
+    </button>
+  </li>
+`;
+
+// ======= references =======
+const refs = {
+  listGroup: document.querySelector('.list-group'),
+  form: document.querySelector('.form-inline'),
+  filter: document.querySelector('#filter'),
+};
+
+// ======= event listeners =======
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.filter.addEventListener('input', handleFilterChange);
+
+render(tasks);
+
+// ======= functions =======
+function handleFilterChange(e) {
+  const filteredTasks = tasks.filter(item =>
+    item.text.toLowerCase().includes(e.target.value.toLowerCase()),
+  );
+
+  render(filteredTasks);
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  if (!e.target.elements.text.value) {
+    return;
+  }
+
+  tasks.push({
+    id: Date.now(),
+    text: e.target.elements.text.value,
+    isDone: false,
+  });
+  e.target.elements.text.value = '';
+
+  saveData();
+  render(tasks);
+}
+
+function render(items) {
+  refs.listGroup.innerHTML = '';
+  refs.listGroup.insertAdjacentHTML(
+    'beforeend',
+    items.map(({ id, text, isDone }) => getItem(id, text, isDone)).join(''),
+  );
+
+  const taskItems = document.querySelectorAll('.list-group-item');
+  taskItems.forEach(elem =>
+    elem.addEventListener('click', handleTaskItemClick),
+  );
+}
+
+function handleTaskItemClick(e) {
+  const { id } = e.currentTarget.dataset;
+
+  if (e.target.nodeName === 'BUTTON') {
+    tasks = tasks.filter(item => item.id !== Number(id));
+  } else {
+    tasks = tasks.map(item => {
+      if (item.id === Number(id)) {
+        return {
+          ...item,
+          isDone: !item.isDone,
+        };
+      }
+
+      return item;
+    });
+  }
+
+  saveData();
+  render(tasks);
+}
+
+function saveData() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
