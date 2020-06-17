d817ae22c79ea4e59cab8d9afb9f6121179d7d49
Geri <gregor.horvath83@gmail.com>
2020-06-17 21:31:03 +0300

    09-webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-'use strict';
+import './style.css';
+
+const sayHello = () => {
+  console.log('Hello');
+};
+
+sayHello();
+
+const data = {
+  users: ['Bobby', 'Peter'],
+};
+const list = document.querySelector('.list');
+list.insertAdjacentHTML(
+  'beforeend',
+  data.users.map(user => `<li class="title">${user}</li>`).join(''),
+);
